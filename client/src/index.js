0bfb0ad4e94414d56a79f2622d4542d67ebc8fe5
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-11-03 18:03:17 -0800

    added nyt api

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import axios from 'axios'
 import App from './components/App';
 
+window.axios = axios
 //const store = createStore({}, {});
 
 ReactDOM.render(<App />, document.getElementById('root'));
