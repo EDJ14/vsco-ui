a8d8284a93bf74a83ed6cbf4521e6ddfdef8075f
Eddie Jacobs <edjacobs@ucdavis.edu>
2019-11-21 17:52:05 -0800

    added city autocomplete search

--- client/src/components/Search/CitySearch.js
+++ client/src/components/Search/CitySearch.js
@@ -1,44 +1,81 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 
 import keys from '../../config/keys';
 
+const CitySuggestBox = styled.div`
+  width: max-content;
+  background-color: black;
+
+  display: flex;
+  flex-direction: column;
+  flex-spacing: space-between;
+`;
+
+const CitySearchItem = styled.div`
+  width: 100%;
+  color: white;
+`;
+
 const CitySearch = props => {
   const [term, setTerm] = useState('');
-
-  let timeout = null;
+  const [timeout, timeoutState] = useState(0);
+  const [showSuggestions, setShowSuggestions] = useState(0);
+  const [suggestions, setSuggestions] = useState([]);
 
   const autoCities = (e, timeout) => {
     setTerm(e.target.value);
+    e.target.value.length == 0 ? setShowSuggestions(0) : setShowSuggestions(1);
 
-    clearTimeout(timeout);
+    if (timeout) {
+      clearTimeout(timeout);
+    }
 
-    timeout = setTimeout(async () => {
-      console.log('setTimeout');
-      const instance = axios.create({
-        baseURL: 'https://wft-geo-db.p.rapidapi.com',
-        timeout: 1000,
-        headers: {
-          'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
-          'x-rapidapi-key': '1c6be6c2c1msh899c6cdcf251ceep17fbafjsn92fd12f82cb0'
-        }
-      });
+    return timeoutState(
+      setTimeout(async () => {
+        console.log('tuneitState');
+        const instance = axios.create({
+          baseURL: 'https://wft-geo-db.p.rapidapi.com',
+          timeout: 1000,
+          headers: {
+            'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+            'x-rapidapi-key':
+              '1c6be6c2c1msh899c6cdcf251ceep17fbafjsn92fd12f82cb0'
+          }
+        });
 
-      const res = await instance.get(
-        `/v1/geo/cities?limit=5&offset=0&namePrefix=${term}`
-      );
+        const res = await instance.get(
+          `/v1/geo/cities?limit=5&offset=0&namePrefix=${term}`
+        );
 
-      console.log('instance', res);
-    }, 500);
+        setSuggestions(res.data.data.map(result => result.city));
+      }, 500)
+    );
   };
 
-  return (
+  const renderCitySuggest = () => {
+    if (!showSuggestions) {
+      return;
+    }
+
+    return (
+      <CitySuggestBox>
+        {suggestions.map(city => (
+          <CitySearchItem>{city}</CitySearchItem>
+        ))}
+      </CitySuggestBox>
+    );
+  };
+
+  return [
     <input
       style={{ margin: '15rem' }}
       value={term}
       onChange={e => autoCities(e, timeout)}
-    />
-  );
+    />,
+    renderCitySuggest()
+  ];
 };
 
 export default CitySearch;
